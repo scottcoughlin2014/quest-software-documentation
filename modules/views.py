85bcb90d1edad14c50bfe6d18f6b612412bbf26f
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2021-03-06 19:37:32 -0600

    Adding example jobs submodule and limiting how many modules we actually track all this info for so that the documentation list is not overwhelming

--- modules/views.py
+++ modules/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Module
 
 # Create your views here.
+MODULES_TO_DISPLAY = [""]
 
 def index(request):
     modules = Module.objects.all()
