f2ca969f67dbb00d2b53d0a21fc62b3f08c313f4
Scott Coughlin <scottcoughlin2014@u.northwestern.edu>
2022-02-25 12:56:40 -0600

    Update nu-software.js

--- nu-software.js
+++ nu-software.js
@@ -1,6 +1,3 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-<script>
 var html = '<div class="container-fluid">';
 html += '<div class="row">';
 html += '<div class="col-sm-1"></div>';
@@ -33,4 +30,3 @@ $(document).ready(function(){
     }
 });
 });
-</script>
