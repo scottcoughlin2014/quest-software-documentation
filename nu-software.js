f61462da81d8033a2dbe966c2be97252c182532c
Scott Coughlin <scottcoughlin2014@u.northwestern.edu>
2022-02-25 12:54:29 -0600

    Create nu-software.js

---
+++ nu-software.js
@@ -0,0 +1,36 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<script>
+var html = '<div class="container-fluid">';
+html += '<div class="row">';
+html += '<div class="col-sm-1"></div>';
+html += '<div class="col-sm-9">';
+html += '<div class="accordion" id="accordionExample">';
+html += '</div>';
+html += '</div>';
+html += '</div>';
+html += '</div>';
+
+
+$(document).ready(function(){   
+    $.ajax({ 
+    type: 'GET', 
+    url: 'https://scottcoughlin2014.github.io/quest-software-documentation/module.json', 
+    dataType: 'json',
+    success: function (data) {    /* Here data length is 5*/
+
+
+         $('#page-content').prepend(html);
+
+         for(var i=0;i<data.length;i++){
+            console.log(data[i].fields.name);
+            var elem = document.createElement('button');
+            elem.className = 'btn btn-link collapsed';
+            elem.innerText = data[i].fields.name;
+            $("#accordionExample").append(elem);
+         }
+
+    }
+});
+});
+</script>
